dd08f87cbbc15509dadc2ccc520db05b7dde474a
Ulukbekislanov <islanovulukbek4@gmail.com>
2026-05-15 23:37:29 +0600

    --

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/MemoryGameApp.dart';
+import 'package:flutter_application_1/home_page.dart';
 
-void main() => runApp(const MemoryGameApp());
+void main() {
+  runApp(const MarketplaceApp());
+}
+
+class MarketplaceApp extends StatelessWidget {
+  const MarketplaceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Marketplace UI',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        fontFamily: 'Arial',
+      ),
+      home: const HomePage(),
+    );
+  }
+}
